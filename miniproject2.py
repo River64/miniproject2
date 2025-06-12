ccfbbd6be0c88ef44155aed61b9a8675c0f0a728
River <https://github.com/River64>
2025-06-12 17:15:08 -0400

    Fixed visited field, added simulation and update function

--- miniproject2.py
+++ miniproject2.py
@@ -6,7 +6,7 @@ def initialize(n, c):
     # Generate random graph with N nodes and C edges
     graph = nx.gnm_random_graph(n, c)
     # Set all nodes to be unvisited
-    nx.set_node_attributes(graph, 0, name = "visited")
+    nx.set_node_attributes(graph, False, name = "visited")
     # Set the starting node- "first" is random, because the graph is randomly generated
     cnode = graph.nodes.first()
     # Add the starting node to the time series
@@ -18,14 +18,31 @@ def observe():
     walk_result.append(cnode)
 
 def update():
-    cnode.visited = 1
-    # choose a random edge from cnode
-    # Set the node on the other side of that edge as the current node
+    # Set our current node as visited
+    cnode.visited = True
+    # Choose a random next node to visit from current node's neighbors
+    cnode = random.choice(cnode.nodes)
 
 # Returns true if all nodes in the graph have been visited by the walker
+# Checked during simulation
 def all_visited():
-    global graph
-    result = true
+    global graph, walk_result
+    result = True
     for node in graph.nodes:
-        result = result & (node.visited == 1)
+        result = result & node.visited
+    # So that the simulation doesn't run forever:
+    if (walk_result.__len__ > 100):
+        result = True
     return result
+
+time_for_average = []
+# Simulate this walking 15 times
+for i in range(15):
+    initialize(10, 10)
+    while(not all_visited()):
+        update()
+        observe()
+    print(walk_result, end = "\n\n")
+    time_for_average.append(walk_result.__len__)
+# Calculate and print average walking time:
+print("Average walking time:", sum(time_for_average) / 15)
